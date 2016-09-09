de1b6c05228bb7cf6b111031a6c7d32349b4bfee
Tyler Dinner <tylerdinner@gmail.com>
2016-09-08 21:33:02 -0400

    init, lesson 1

---
+++ script01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# Welcome to lesson 1! 
+# Lines beginning with a '#' are comments
+
+# *IMPORTANT!* 
+# TO RUN THIS SCRIPT, first run this in the terminal
+# $ chmod +x myscript.sh 
+# Then to run it: 
+# $ ./script01.sh
+# This gives you permission to execute this file
+
+echo "Hello World"
+
