221a10c2841626cbfd24b729c03da321223f9e8d
Tyler Dinner <tylerdinner@gmail.com>
2016-09-09 15:14:53 -0400

    script 3 and 4

---
+++ script03.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Use > to pipe something. 
+
+# This command takes takes the output of 'ls' 
+# and writes it in a new file named myLs.txt
+
+ls > myLs.txt
---
+++ script04.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# in bash, 1 is standard out (stdout) and 2 is stderr
+
+# this script will send the error to myError.txt
+4 + 3r 2> myError.txt
