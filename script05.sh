b0a772bc84b7af3c7702ce730059106665ee04dc
Tyler Dinner <tylerdinner@gmail.com>
2016-09-09 18:39:33 -0400

    script 456

---
+++ script05.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Functions!
+# Make a function like this:
+function sayHelloFunction {
+  echo "Hello"
+}
+
+# Call sayHelloFunction
+
+sayHelloFunction
+
+
+# if/else statements
+
+if [ "bob" = "bob" ]; then
+  echo "it's bob!"
+else
+  echo "it's not bob!"
+fi
---
+++ script06.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# for loops!!
+
+for i in $( ls ); do
+  echo result: $i
+done
+
+
+for i in `seq 0 5`;
+do
+  echo i: $i
+done 
