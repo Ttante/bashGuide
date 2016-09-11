8c13ad3ef590e014165a1e5db74ba54480304510
Tyler Dinner <tylerdinner@gmail.com>
2016-09-11 14:55:12 -0400

    scripts 09 and update 5

--- script05.sh
+++ script05.sh
@@ -4,15 +4,19 @@
 # Make a function like this:
 function sayHelloFunction {
   echo Hello
+  return 0
 }
 
 # Call sayHelloFunction
-
 sayHelloFunction
 
+# In bash, functions always return
+# a number. 0 means everything
+# is OK in bash. It's what we want!
 
 function sayThis {
   echo this: $1
+  return 0
 }
 
 sayThis "Random Words"
--- script09.sh
+++ script09.sh
@@ -4,3 +4,21 @@
 # grep searches files for phrase matches
 
 echo grep ./*
+
+
+word1="dog"
+word2="cat"
+word3=word1
+
+# Ways to compare strings:
+
+echo $((word1 == word1))
+echo $((word1 != word2))
+
+# There are flags for the basic comparison operators too
+# >  -gt  (greater than)
+# <  -lt  (less than)
+# >= -ge  (greater than or equal to)
+# <= -le  (less than or equal to)
+# == -eq  (equal)
+# != -ne  (not equal)
